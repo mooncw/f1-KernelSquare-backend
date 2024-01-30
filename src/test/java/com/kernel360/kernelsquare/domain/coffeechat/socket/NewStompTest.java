ae7d8ff4e44f8bed72696f0f22b344126d651baf
mooncw <dusrktl4@gmail.com>
2024-01-30 17:15:27 +0900

    [chore] local_test 9차

--- src/test/java/com/kernel360/kernelsquare/domain/coffeechat/socket/NewStompTest.java
+++ src/test/java/com/kernel360/kernelsquare/domain/coffeechat/socket/NewStompTest.java
@@ -1,127 +1,120 @@
-package com.kernel360.kernelsquare.domain.coffeechat.socket;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.kernel360.kernelsquare.domain.coffeechat.dto.ChatMessage;
-import com.kernel360.kernelsquare.domain.coffeechat.dto.MessageType;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.*;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.socket.WebSocketHttpHeaders;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.Transport;
-import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@ExtendWith(SpringExtension.class)
-@DisplayName("로거 되냐 ? ?  ? ? ?? ? ? ? ? ? ? ")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class NewStompTest {
-    Logger logger = LoggerFactory.getLogger(NewStompTest.class);
-
-    @LocalServerPort
-    private int port;
-
-    private SockJsClient sockJsClient;
-
-    private WebSocketStompClient stompClient;
-
-    private final WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
-
-    @BeforeEach
-    public void setup() {
-        List<Transport> transports = new ArrayList<>();
-        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
-
-        this.sockJsClient = new SockJsClient(transports);
-        this.stompClient = new WebSocketStompClient(sockJsClient);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-
-        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
-        messageConverter.setObjectMapper(objectMapper);
-
-        this.stompClient.setMessageConverter(messageConverter);
-    }
-
-    @Test
-    public void getGreeting() throws Exception {
-
-        final CountDownLatch latch = new CountDownLatch(1);
-        StompSessionHandler handler = new TestSessionHandler(latch);
-        this.stompClient.connect("ws://localhost:" + this.port + "/ws", this.headers, handler);
-
-        long start = System.currentTimeMillis();
-
-        logger.info("시작");
-
-        latch.await(30, TimeUnit.SECONDS);
-
-        logger.info("끝 : " + (System.currentTimeMillis() - start));
-
-        assertThat(latch.getCount()).isEqualTo(0);
-    }
-
-    class TestSessionHandler extends StompSessionHandlerAdapter {
-
-        private final CountDownLatch latch;
-
-        public TestSessionHandler(CountDownLatch latch) {
-            this.latch = latch;
-        }
-
-        @Override
-        public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-            ChatMessage sendMessage = ChatMessage.builder()
-                .message("hi")
-                .roomKey("key")
-                .type(MessageType.TALK)
-                .sender("홍박사")
-                .build();
-
-            session.subscribe("/topic/test/room/" + sendMessage.getRoomKey(), new StompFrameHandler() {
-                @Override
-                public Type getPayloadType(StompHeaders headers) {
-                    return ChatMessage.class;
-                }
-
-                @Override
-                public void handleFrame(StompHeaders headers, Object payload) {
-                    ChatMessage receiveMessage = (ChatMessage) payload;
-
-                    System.out.println(receiveMessage.getRoomKey());
-
-                    try {
-                        assertThat(receiveMessage.getMessage()).startsWith("hi");
-                        latch.countDown();
-                    } catch (Throwable e) {
-                        latch.countDown();
-                    }
-                }
-            });
-            try {
-                session.send("/app/test/message", sendMessage);
-            } catch (Throwable t) {
-                latch.countDown();
-            }
-        }
-    }
-}
+//package com.kernel360.kernelsquare.domain.coffeechat.socket;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+//import com.kernel360.kernelsquare.domain.coffeechat.dto.ChatMessage;
+//import com.kernel360.kernelsquare.domain.coffeechat.dto.MessageType;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.server.LocalServerPort;
+//import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+//import org.springframework.messaging.simp.stomp.*;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.web.socket.WebSocketHttpHeaders;
+//import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+//import org.springframework.web.socket.messaging.WebSocketStompClient;
+//import org.springframework.web.socket.sockjs.client.SockJsClient;
+//import org.springframework.web.socket.sockjs.client.Transport;
+//import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+//
+//import java.lang.reflect.Type;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.concurrent.CountDownLatch;
+//import java.util.concurrent.TimeUnit;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@ExtendWith(SpringExtension.class)
+//@DisplayName("로거 되냐 ? ?  ? ? ?? ? ? ? ? ? ? ")
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class NewStompTest {
+//
+//    @LocalServerPort
+//    private int port;
+//
+//    private SockJsClient sockJsClient;
+//
+//    private WebSocketStompClient stompClient;
+//
+//    private final WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+//
+//    @BeforeEach
+//    public void setup() {
+//        List<Transport> transports = new ArrayList<>();
+//        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+//
+//        this.sockJsClient = new SockJsClient(transports);
+//        this.stompClient = new WebSocketStompClient(sockJsClient);
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+//
+//        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+//        messageConverter.setObjectMapper(objectMapper);
+//
+//        this.stompClient.setMessageConverter(messageConverter);
+//    }
+//
+//    @Test
+//    public void getGreeting() throws Exception {
+//
+//        final CountDownLatch latch = new CountDownLatch(1);
+//        StompSessionHandler handler = new TestSessionHandler(latch);
+//        this.stompClient.connect("ws://localhost:" + this.port + "/ws", this.headers, handler);
+//
+//        latch.await(30, TimeUnit.SECONDS);
+//
+//        assertThat(latch.getCount()).isEqualTo(0);
+//    }
+//
+//    class TestSessionHandler extends StompSessionHandlerAdapter {
+//
+//        private final CountDownLatch latch;
+//
+//        public TestSessionHandler(CountDownLatch latch) {
+//            this.latch = latch;
+//        }
+//
+//        @Override
+//        public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+//            ChatMessage sendMessage = ChatMessage.builder()
+//                .message("hi")
+//                .roomKey("key")
+//                .type(MessageType.TALK)
+//                .sender("홍박사")
+//                .build();
+//
+//            session.subscribe("/topic/test/room/" + sendMessage.getRoomKey(), new StompFrameHandler() {
+//                @Override
+//                public Type getPayloadType(StompHeaders headers) {
+//                    return ChatMessage.class;
+//                }
+//
+//                @Override
+//                public void handleFrame(StompHeaders headers, Object payload) {
+//                    ChatMessage receiveMessage = (ChatMessage) payload;
+//
+//                    System.out.println(receiveMessage.getRoomKey());
+//
+//                    try {
+//                        assertThat(receiveMessage.getMessage()).startsWith("hi");
+//                        latch.countDown();
+//                    } catch (Throwable e) {
+//                        latch.countDown();
+//                    }
+//                }
+//            });
+//            try {
+//                session.send("/app/test/message", sendMessage);
+//            } catch (Throwable t) {
+//                latch.countDown();
+//            }
+//        }
+//    }
+//}
--- src/test/java/com/kernel360/kernelsquare/domain/coffeechat/socket/StompSocketTest.java
+++ src/test/java/com/kernel360/kernelsquare/domain/coffeechat/socket/StompSocketTest.java
@@ -1,144 +1,115 @@
-//package com.kernel360.kernelsquare.domain.coffeechat.socket;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-//import com.kernel360.kernelsquare.domain.coffeechat.dto.ChatMessage;
-//import com.kernel360.kernelsquare.domain.coffeechat.dto.MessageType;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.server.LocalServerPort;
-//import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-//import org.springframework.messaging.simp.stomp.StompHeaders;
-//import org.springframework.messaging.simp.stomp.StompSession;
-//import org.springframework.messaging.simp.stomp.StompSessionHandler;
-//import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-//import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-//import org.springframework.web.socket.messaging.WebSocketStompClient;
-//import org.springframework.web.socket.sockjs.client.SockJsClient;
-//import org.springframework.web.socket.sockjs.client.Transport;
-//import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-//
-//import java.lang.reflect.Type;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.concurrent.*;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@DisplayName("STOMP 소켓 통신 테스트")
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//public class StompSocketTest {
-//    Logger logger = LoggerFactory.getLogger(StompSocketTest.class);
-//
-//    protected StompSession stompSession;
-//
-//    @LocalServerPort
-//    private int port;
-//
-//    private final String url;
-//
-//    private final String endPoint;
-//
-//    private final WebSocketStompClient websocketClient;
-//
-//    private final BlockingQueue<ChatMessage> blockingQueue;
-//
-//    private final StompSessionHandler sessionHandler;
-//
-//
-//    public StompSocketTest() {
-//        this.blockingQueue = new LinkedBlockingQueue<>();
-//
-//        this.sessionHandler = new TestSessionHandler(blockingQueue);
-//
-//        this.url = "ws://localhost:";
-//
-//        this.endPoint = "/kernel-square";
-//
-//        this.websocketClient = new WebSocketStompClient(new SockJsClient(createTransport()));
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-//
-//        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
-//        messageConverter.setObjectMapper(objectMapper);
-//
-//        this.websocketClient.setMessageConverter(messageConverter);
-//    }
-//
-//    @BeforeEach
-//    public void connect() throws ExecutionException, InterruptedException, TimeoutException {
-//        logger.info("연결 시작");
-//        var start = System.currentTimeMillis();
-//        this.stompSession = this.websocketClient
-//            .connect(url + port + endPoint, this.sessionHandler)
-//            .get(100, TimeUnit.SECONDS);
-//        logger.info("연결 완료 : " + (System.currentTimeMillis() - start));
-//    }
-//
-//    @AfterEach
-//    public void disconnect() {
-//        if (this.stompSession.isConnected()) {
-//            this.stompSession.disconnect();
-//        }
-//    }
-//
-//    private List<Transport> createTransport() {
-//        List<Transport> transports = new ArrayList<>(1);
-//        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
-//        return transports;
-//    }
-//
-//    private class TestSessionHandler extends StompSessionHandlerAdapter {
-//
-//        private final BlockingQueue<ChatMessage> blockingQueue;
-//
-//        public TestSessionHandler(BlockingQueue<ChatMessage> blockingQueue) {
-//            this.blockingQueue = blockingQueue;
-//        }
-//
-//        @Override
-//        public Type getPayloadType(StompHeaders headers) {
-//            return ChatMessage.class;
-//        }
-//
-//        @Override
-//        public void handleFrame(StompHeaders headers, Object payload) {
-//            blockingQueue.offer((ChatMessage) payload);
-//        }
-//    }
-//
-//    @Test
-//    @DisplayName("메시지 전송 테스트")
-//    public void testSendMessage() throws InterruptedException {
-//        //given
-//        ChatMessage message = ChatMessage.builder()
-//            .message("hi")
-//            .roomKey("key")
-//            .type(MessageType.TALK)
-//            .sender("홍박사")
-//            .build();
-//
-//        this.stompSession.subscribe("/topic/chat/room/" + message.getRoomKey(), this.sessionHandler);
-//
-//        //when
-//        this.stompSession.send("/app/chat/message", message);
-//
-//        logger.info("메시지 수신 시작");
-//        var start = System.currentTimeMillis();
-//        ChatMessage receivedMessage = blockingQueue.poll(100, TimeUnit.SECONDS);
-//        logger.info("메시지 수신 종료 : " + (System.currentTimeMillis() - start));
-//
-//        //then
-//        assertThat(receivedMessage).isNotNull();
-//        assertThat(receivedMessage.getMessage()).isEqualTo(message.getMessage());
-//        assertThat(receivedMessage.getSender()).isEqualTo(message.getSender());
-//        assertThat(receivedMessage.getRoomKey()).isEqualTo(message.getRoomKey());
-//        assertThat(receivedMessage.getType()).isEqualTo(message.getType());
-//    }
-//}
+package com.kernel360.kernelsquare.domain.coffeechat.socket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.kernel360.kernelsquare.domain.coffeechat.dto.ChatMessage;
+import com.kernel360.kernelsquare.domain.coffeechat.dto.MessageType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.*;
+import org.springframework.web.socket.WebSocketHttpHeaders;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("STOMP 소켓 통신 테스트")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StompSocketTest {
+    @LocalServerPort
+    private int port;
+
+    private SockJsClient sockJsClient;
+
+    private WebSocketStompClient stompClient;
+
+    private final WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+
+    @BeforeEach
+    public void setup() {
+        List<Transport> transports = new ArrayList<>();
+        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+
+        this.sockJsClient = new SockJsClient(transports);
+        this.stompClient = new WebSocketStompClient(sockJsClient);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setObjectMapper(objectMapper);
+
+        this.stompClient.setMessageConverter(messageConverter);
+    }
+
+    @Test
+    @DisplayName("메시지 수신 테스트")
+    public void getMessage() throws Exception {
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        StompSessionHandler handler = new TestSessionHandler(latch);
+        this.stompClient.connect("ws://localhost:" + this.port + "/ws", this.headers, handler);
+
+        latch.await(30, TimeUnit.SECONDS);
+
+        assertThat(latch.getCount()).isEqualTo(0);
+    }
+
+    class TestSessionHandler extends StompSessionHandlerAdapter {
+
+        private final CountDownLatch latch;
+
+        public TestSessionHandler(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        @Override
+        public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+            ChatMessage sendMessage = ChatMessage.builder()
+                .message("hi")
+                .roomKey("key")
+                .type(MessageType.TALK)
+                .sender("홍박사")
+                .build();
+
+            session.subscribe("/topic/test/room/" + sendMessage.getRoomKey(), new StompFrameHandler() {
+                @Override
+                public Type getPayloadType(StompHeaders headers) {
+                    return ChatMessage.class;
+                }
+
+                @Override
+                public void handleFrame(StompHeaders headers, Object payload) {
+                    ChatMessage receiveMessage = (ChatMessage) payload;
+
+                    System.out.println(receiveMessage.getRoomKey());
+
+                    try {
+                        assertThat(receiveMessage.getMessage()).startsWith("hi");
+                        latch.countDown();
+                    } catch (Throwable e) {
+                        latch.countDown();
+                    }
+                }
+            });
+            try {
+                session.send("/app/test/message", sendMessage);
+            } catch (Throwable t) {
+                latch.countDown();
+            }
+        }
+    }
+}
