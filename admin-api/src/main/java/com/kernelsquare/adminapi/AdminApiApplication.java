f011388f2e9ef474ab2b36c2fd3d09ccd7d053a4
mooncw <dusrktl4@gmail.com>
2024-04-19 17:52:02 +0900

    refactor: admin-api 모듈 설정 현재 repo에 맞게 수정
    - AdminApiApplication이 스캔하는 패키지에 domains3와 domainredis 추가
    - build.gradle 모듈 추가 및 펼요 없는 부분 삭제
    - admin-api 모듈에 있는 필요없는 Config 삭제
    - 멤버 서비스를 위한 Search 도메인 삭제 -> 추후 어드민에 맞게 추가 예정

--- admin-api/src/main/java/com/kernelsquare/adminapi/AdminApiApplication.java
+++ admin-api/src/main/java/com/kernelsquare/adminapi/AdminApiApplication.java
@@ -3,8 +3,12 @@ package com.kernelsquare.adminapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.kernelsquare.core",
-	"com.kernelsquare.adminapi", "com.kernelsquare.domainmysql"})
+@SpringBootApplication(scanBasePackages = {
+	"com.kernelsquare.core",
+	"com.kernelsquare.adminapi",
+	"com.kernelsquare.domainmysql",
+	"com.kernelsquare.domains3",
+	"com.kernelsquare.domainredis"})
 public class AdminApiApplication {
 
 	public static void main(String[] args) {
