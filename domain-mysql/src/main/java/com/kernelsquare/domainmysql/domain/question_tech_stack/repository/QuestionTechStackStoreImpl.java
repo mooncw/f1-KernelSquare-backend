fec5ddce81b757375773613ddf48465007d72828
mooncw <dusrktl4@gmail.com>
2024-05-01 22:30:02 +0900

    refactor: domain-mysql 모듈 question_tech_stack 레포지토리 DI 적용 추가-1

--- domain-mysql/src/main/java/com/kernelsquare/domainmysql/domain/question_tech_stack/repository/QuestionTechStackStoreImpl.java
+++ domain-mysql/src/main/java/com/kernelsquare/domainmysql/domain/question_tech_stack/repository/QuestionTechStackStoreImpl.java
@@ -1,5 +1,6 @@
 package com.kernelsquare.domainmysql.domain.question_tech_stack.repository;
 
+import com.kernelsquare.domainmysql.domain.question_tech_stack.entity.QuestionTechStack;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +8,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class QuestionTechStackStoreImpl implements QuestionTechStackStore {
     private final QuestionTechStackRepository questionTechStackRepository;
+
+    @Override
+    public void store(QuestionTechStack questionTechStack) {
+        questionTechStackRepository.save(questionTechStack);
+    }
 
     @Override
     public void deleteAll(Long questionId) {
