d59628cbf42954b814e5d2ea84f3b3a7a64edf49
mooncw <dusrktl4@gmail.com>
2024-05-01 20:36:14 +0900

    refactor: DI 적용에 따른 admin-api 모듈 tech_stack 서비스 리팩토링

--- admin-api/src/main/java/com/kernelsquare/adminapi/domain/tech_stack/service/TechStackService.java
+++ admin-api/src/main/java/com/kernelsquare/adminapi/domain/tech_stack/service/TechStackService.java
@@ -1,11 +1,5 @@
 package com.kernelsquare.adminapi.domain.tech_stack.service;
 
-import java.util.List;
-
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.kernelsquare.adminapi.domain.tech_stack.dto.CreateTechStackRequest;
 import com.kernelsquare.adminapi.domain.tech_stack.dto.CreateTechStackResponse;
 import com.kernelsquare.adminapi.domain.tech_stack.dto.FindAllTechStacksResponse;
@@ -13,19 +7,26 @@ import com.kernelsquare.adminapi.domain.tech_stack.dto.UpdateTechStackRequest;
 import com.kernelsquare.core.common_response.error.code.TechStackErrorCode;
 import com.kernelsquare.core.common_response.error.exception.BusinessException;
 import com.kernelsquare.domainmysql.domain.tech_stack.entity.TechStack;
-import com.kernelsquare.domainmysql.domain.tech_stack.repository.TechStackRepository;
-
+import com.kernelsquare.domainmysql.domain.tech_stack.repository.TechStackReader;
+import com.kernelsquare.domainmysql.domain.tech_stack.repository.TechStackStore;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TechStackService {
-	private final TechStackRepository techStackRepository;
+	private final TechStackReader techStackReader;
+	private final TechStackStore techStackStore;
 
+	@Transactional
 	public CreateTechStackResponse createTechStack(CreateTechStackRequest createTechStackRequest) {
 		TechStack techStack = CreateTechStackRequest.toEntity(createTechStackRequest);
 		try {
-			TechStack saveTechStack = techStackRepository.save(techStack);
+			TechStack saveTechStack = techStackStore.store(techStack);
 
 			return CreateTechStackResponse.from(saveTechStack);
 		} catch (DataIntegrityViolationException e) {
@@ -35,17 +36,16 @@ public class TechStackService {
 
 	@Transactional(readOnly = true)
 	public FindAllTechStacksResponse findAllTechStacks() {
-		List<TechStack> techStackList = techStackRepository.findAll();
+		List<TechStack> techStackList = techStackReader.findAll();
 
 		return FindAllTechStacksResponse.from(techStackList);
 	}
 
 	@Transactional
 	public void updateTechStack(Long techStackId, UpdateTechStackRequest updateTechStackRequest) {
-		TechStack techStack = techStackRepository.findById(techStackId)
-			.orElseThrow(() -> new BusinessException(TechStackErrorCode.TECH_STACK_NOT_FOUND));
+		TechStack techStack = techStackReader.find(techStackId);
 
-		boolean isSkillExists = techStackRepository.existsBySkill(updateTechStackRequest.skill());
+		boolean isSkillExists = techStackReader.existsBySkill(updateTechStackRequest.skill());
 
 		if (isSkillExists) {
 			throw new BusinessException(TechStackErrorCode.TECH_STACK_ALREADY_EXISTED);
@@ -56,6 +56,6 @@ public class TechStackService {
 
 	@Transactional
 	public void deleteTechStack(Long techStackId) {
-		techStackRepository.deleteById(techStackId);
+		techStackStore.delete(techStackId);
 	}
 }
