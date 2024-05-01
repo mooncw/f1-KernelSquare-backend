fb424a6799d7d555c3a0ba1d89786b0e51ac96a3
mooncw <dusrktl4@gmail.com>
2024-05-01 21:26:49 +0900

    refactor: DI 적용에 따른 member-api 모듈 AuthService 리팩토링

--- member-api/src/main/java/com/kernelsquare/memberapi/domain/auth/service/AuthService.java
+++ member-api/src/main/java/com/kernelsquare/memberapi/domain/auth/service/AuthService.java
@@ -1,23 +1,21 @@
 package com.kernelsquare.memberapi.domain.auth.service;
 
 import com.kernelsquare.core.common_response.error.code.AuthErrorCode;
-import com.kernelsquare.core.common_response.error.code.AuthorityErrorCode;
-import com.kernelsquare.core.common_response.error.code.LevelErrorCode;
 import com.kernelsquare.core.common_response.error.exception.BusinessException;
 import com.kernelsquare.core.type.AuthorityType;
 import com.kernelsquare.core.util.ExperiencePolicy;
 import com.kernelsquare.domainmysql.domain.auth.command.AuthCommand;
 import com.kernelsquare.domainmysql.domain.auth.info.AuthInfo;
 import com.kernelsquare.domainmysql.domain.authority.entity.Authority;
-import com.kernelsquare.domainmysql.domain.authority.repository.AuthorityRepository;
+import com.kernelsquare.domainmysql.domain.authority.repository.AuthorityReader;
 import com.kernelsquare.domainmysql.domain.level.entity.Level;
-import com.kernelsquare.domainmysql.domain.level.repository.LevelRepository;
+import com.kernelsquare.domainmysql.domain.level.repository.LevelReader;
 import com.kernelsquare.domainmysql.domain.member.entity.Member;
 import com.kernelsquare.domainmysql.domain.member.info.MemberInfo;
 import com.kernelsquare.domainmysql.domain.member.repository.MemberReader;
-import com.kernelsquare.domainmysql.domain.member.repository.MemberRepository;
+import com.kernelsquare.domainmysql.domain.member.repository.MemberStore;
 import com.kernelsquare.domainmysql.domain.member_authority.entity.MemberAuthority;
-import com.kernelsquare.domainmysql.domain.member_authority.repository.MemberAuthorityRepository;
+import com.kernelsquare.domainmysql.domain.member_authority.repository.MemberAuthorityStore;
 import com.kernelsquare.memberapi.domain.auth.dto.SignUpRequest;
 import com.kernelsquare.memberapi.domain.auth.dto.SignUpResponse;
 import com.kernelsquare.memberapi.domain.auth.validation.AuthValidation;
@@ -32,11 +30,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthService {
 	private final PasswordEncoder passwordEncoder;
-	private final MemberRepository memberRepository;
-	private final MemberAuthorityRepository memberAuthorityRepository;
-	private final AuthorityRepository authorityRepository;
-	private final LevelRepository levelRepository;
+	private final MemberAuthorityStore memberAuthorityStore;
+	private final AuthorityReader authorityReader;
+	private final LevelReader levelReader;
 	private final MemberReader memberReader;
+	private final MemberStore memberStore;
 	private final TokenProvider tokenProvider;
 	private final AuthValidation authValidation;
 
@@ -53,15 +51,14 @@ public class AuthService {
 
 	@Transactional
 	public SignUpResponse signUp(final SignUpRequest signUpRequest) {
-		Level level = levelRepository.findByName(1L)
-			.orElseThrow(() -> new BusinessException(LevelErrorCode.LEVEL_NOT_FOUND));
-		String encodedPassword = passwordEncoder.encode(signUpRequest.password());
-		Member savedMember = memberRepository.save(SignUpRequest.toEntity(signUpRequest, encodedPassword, level));
+		Level level = levelReader.findByLevelName(1L);
 
-		Authority authority = authorityRepository.findAuthorityByAuthorityType(AuthorityType.ROLE_USER)
-			.orElseThrow(() -> new BusinessException(AuthorityErrorCode.AUTHORITY_NOT_FOUND));
+		String encodedPassword = passwordEncoder.encode(signUpRequest.password());
+		Member savedMember = memberStore.store(SignUpRequest.toEntity(signUpRequest, encodedPassword, level));
+
+		Authority authority = authorityReader.findAuthority(AuthorityType.ROLE_USER);
 		MemberAuthority memberAuthority = MemberAuthority.builder().member(savedMember).authority(authority).build();
-		memberAuthorityRepository.save(memberAuthority);
+		memberAuthorityStore.store(memberAuthority);
 
 		List<MemberAuthority> authorities = List.of(memberAuthority);
 		savedMember.initAuthorities(authorities);
@@ -71,14 +68,14 @@ public class AuthService {
 
 	@Transactional(readOnly = true)
 	public void isEmailUnique(final String email) {
-		if (memberRepository.existsByEmail(email)) {
+		if (memberReader.existsMemberEmail(email)) {
 			throw new BusinessException(AuthErrorCode.ALREADY_SAVED_EMAIL);
 		}
 	}
 
 	@Transactional(readOnly = true)
 	public void isNicknameUnique(final String nickname) {
-		if (memberRepository.existsByNickname(nickname)) {
+		if (memberReader.existsMemberNickname(nickname)) {
 			throw new BusinessException(AuthErrorCode.ALREADY_SAVED_NICKNAME);
 		}
 	}
@@ -87,8 +84,7 @@ public class AuthService {
 		member.addExperience(ExperiencePolicy.MEMBER_DAILY_ATTENDED.getReward());
 		if (member.isExperienceExceed(member.getExperience())) {
 			member.updateExperience(member.getExperience() - member.getLevel().getLevelUpperLimit());
-			Level nextLevel = levelRepository.findByName(member.getLevel().getName() + 1)
-				.orElseThrow(() -> new BusinessException(LevelErrorCode.LEVEL_NOT_FOUND));
+			Level nextLevel = levelReader.findByLevelName(member.getLevel().getName() + 1);
 			member.updateLevel(nextLevel);
 		}
 	}
