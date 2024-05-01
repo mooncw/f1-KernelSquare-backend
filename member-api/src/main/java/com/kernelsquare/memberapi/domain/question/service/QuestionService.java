4f07be047af0a13dd40caa03ee5052e5f2889e58
mooncw <dusrktl4@gmail.com>
2024-05-01 22:30:31 +0900

    refactor: DI 적용에 따른 member-api 모듈 QuestionService 리팩토링

--- member-api/src/main/java/com/kernelsquare/memberapi/domain/question/service/QuestionService.java
+++ member-api/src/main/java/com/kernelsquare/memberapi/domain/question/service/QuestionService.java
@@ -1,7 +1,5 @@
 package com.kernelsquare.memberapi.domain.question.service;
 
-import com.kernelsquare.core.common_response.error.code.LevelErrorCode;
-import com.kernelsquare.core.common_response.error.code.MemberErrorCode;
 import com.kernelsquare.core.common_response.error.code.QuestionErrorCode;
 import com.kernelsquare.core.common_response.error.exception.BusinessException;
 import com.kernelsquare.core.dto.PageResponse;
@@ -9,17 +7,17 @@ import com.kernelsquare.core.dto.Pagination;
 import com.kernelsquare.core.util.ExperiencePolicy;
 import com.kernelsquare.core.util.ImageUtils;
 import com.kernelsquare.domainmysql.domain.level.entity.Level;
-import com.kernelsquare.domainmysql.domain.level.repository.LevelRepository;
+import com.kernelsquare.domainmysql.domain.level.repository.LevelReader;
 import com.kernelsquare.domainmysql.domain.member.entity.Member;
-import com.kernelsquare.domainmysql.domain.member.repository.MemberRepository;
+import com.kernelsquare.domainmysql.domain.member.repository.MemberReader;
 import com.kernelsquare.domainmysql.domain.question.entity.Question;
 import com.kernelsquare.domainmysql.domain.question.info.QuestionInfo;
 import com.kernelsquare.domainmysql.domain.question.repository.QuestionReader;
-import com.kernelsquare.domainmysql.domain.question.repository.QuestionRepository;
+import com.kernelsquare.domainmysql.domain.question.repository.QuestionStore;
 import com.kernelsquare.domainmysql.domain.question_tech_stack.entity.QuestionTechStack;
-import com.kernelsquare.domainmysql.domain.question_tech_stack.repository.QuestionTechStackRepository;
+import com.kernelsquare.domainmysql.domain.question_tech_stack.repository.QuestionTechStackStore;
 import com.kernelsquare.domainmysql.domain.tech_stack.entity.TechStack;
-import com.kernelsquare.domainmysql.domain.tech_stack.repository.TechStackRepository;
+import com.kernelsquare.domainmysql.domain.tech_stack.repository.TechStackReader;
 import com.kernelsquare.memberapi.domain.question.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -33,20 +31,19 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
-	private final QuestionRepository questionRepository;
-	private final MemberRepository memberRepository;
-	private final TechStackRepository techStackRepository;
-	private final QuestionTechStackRepository questionTechStackRepository;
-	private final LevelRepository levelRepository;
 	private final QuestionReader questionReader;
+	private final QuestionStore questionStore;
+	private final MemberReader memberReader;
+	private final TechStackReader techStackReader;
+	private final QuestionTechStackStore questionTechStackStore;
+	private final LevelReader levelReader;
 
 	@Transactional
 	public CreateQuestionResponse createQuestion(CreateQuestionRequest createQuestionRequest) {
-		Member member = memberRepository.findById(createQuestionRequest.memberId())
-			.orElseThrow(() -> new BusinessException(MemberErrorCode.MEMBER_NOT_FOUND));
+		Member member = memberReader.findMember(createQuestionRequest.memberId());
 
 		Question question = CreateQuestionRequest.toEntity(createQuestionRequest, member);
-		Question saveQuestion = questionRepository.save(question);
+		Question saveQuestion = questionStore.store(question);
 
 		List<String> skills = createQuestionRequest.skills();
 		List<QuestionTechStack> techStackList = new ArrayList<>();
@@ -56,8 +53,7 @@ public class QuestionService {
 		member.addExperience(ExperiencePolicy.QUESTION_CREATED.getReward());
 		if (member.isExperienceExceed(member.getExperience())) {
 			member.updateExperience(member.getExperience() - member.getLevel().getLevelUpperLimit());
-			Level nextLevel = levelRepository.findByName(member.getLevel().getName() + 1)
-				.orElseThrow(() -> new BusinessException(LevelErrorCode.LEVEL_NOT_FOUND));
+			Level nextLevel = levelReader.findByLevelName(member.getLevel().getName() + 1);
 			member.updateLevel(nextLevel);
 		}
 		return CreateQuestionResponse.from(saveQuestion);
@@ -67,8 +63,7 @@ public class QuestionService {
 	public FindQuestionResponse findQuestion(
 		Long questionId
 	) {
-		Question question = questionRepository.findById(questionId)
-			.orElseThrow(() -> new BusinessException(QuestionErrorCode.QUESTION_NOT_FOUND));
+		Question question = questionReader.findQuestion(questionId);
 
 		Member member = question.getMember();
 
@@ -102,7 +97,7 @@ public class QuestionService {
 
 	@Transactional(readOnly = true)
 	public FindAllQuestionResponse findAllQuestionsSeo() {
-		return FindAllQuestionResponse.of(questionRepository.findAll().stream()
+		return FindAllQuestionResponse.of(questionReader.findAll().stream()
 				.map(Question::getId)
 				.map(FindQuestionIdResponse::of)
 				.toList());
@@ -110,8 +105,7 @@ public class QuestionService {
 
 	@Transactional
 	public void updateQuestion(Long questionId, UpdateQuestionRequest updateQuestionRequest) {
-		Question question = questionRepository.findById(questionId)
-			.orElseThrow(() -> new BusinessException(QuestionErrorCode.QUESTION_NOT_FOUND));
+		Question question = questionReader.findQuestion(questionId);
 
 		question.update(updateQuestionRequest.title(), updateQuestionRequest.content(),
 			ImageUtils.parseFilePath(updateQuestionRequest.imageUrl()));
@@ -119,21 +113,21 @@ public class QuestionService {
 		List<String> skills = updateQuestionRequest.skills();
 		List<QuestionTechStack> techStackList = new ArrayList<>();
 
-		questionTechStackRepository.deleteAllByQuestionId(questionId);
+		questionTechStackStore.deleteAll(questionId);
 
 		saveTechStackList(question, skills, techStackList);
 	}
 
 	private void saveTechStackList(Question question, List<String> skills, List<QuestionTechStack> techStackList) {
 		for (String skill : skills) {
-			TechStack techStack = techStackRepository.findBySkill(skill).orElseThrow(() -> new RuntimeException());
+			TechStack techStack = techStackReader.findBySkill(skill);
 
 			QuestionTechStack questionTechStack = QuestionTechStack.builder()
 				.techStack(techStack)
 				.question(question)
 				.build();
 
-			questionTechStackRepository.save(questionTechStack);
+			questionTechStackStore.store(questionTechStack);
 			techStackList.add(questionTechStack);
 		}
 		question.setTechStackList(techStackList);
@@ -141,11 +135,10 @@ public class QuestionService {
 
 	@Transactional
 	public void deleteQuestion(Long questionId) {
-		questionRepository.findById(questionId)
-			.orElseThrow(() -> new BusinessException(QuestionErrorCode.QUESTION_NOT_FOUND));
+		questionReader.findQuestion(questionId);
 
-		questionRepository.deleteById(questionId);
+		questionStore.delete(questionId);
 
-		questionTechStackRepository.deleteAllByQuestionId(questionId);
+		questionTechStackStore.deleteAll(questionId);
 	}
 }
